19536e045f2597febc3570f8040b91ca62987f1b
keabll1 <lee.keable@aviva.com>
2018-10-05 09:03:04 +0100

    get something running, added a Lexer

---
+++ mc/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace mc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("> ");
+            var line = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(line))
+                return;
+
+        }
+    }
+
+    class Lexer
+    {
+        private readonly string _text;
+
+        public Lexer(string text)
+        {
+            _text = text;
+        }
+    }
+}
